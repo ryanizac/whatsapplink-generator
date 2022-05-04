b3d265cefd17743d595ba9f0314d137444e927c7
Ryan <io.ryan@outlook.com>
2022-05-03 22:49:28 -0300

    whatsapp image added

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -6,6 +6,7 @@ interface ContentProps {}
 export default function Content(props: ContentProps) {
   return (
     <div className="content">
+      <img src="/images/whatsapp.png" alt="whatsappimage" />
       <h1>Whatsapp Link - Generator</h1>
       <Form />
     </div>
